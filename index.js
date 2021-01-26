c45e1809f9598f531f41ea918036a48670b3245e
Fabrizio <77286525+fabriziomicucci@users.noreply.github.com>
2021-01-26 14:42:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+// Task 1
+
+let a = 3;
+let b = 5;
+let c;
+
+let output = '';
+output += 'let a = 3;';
+output += '\n';
+output += 'let b = 5;';
+output += '\n';
+output += 'let c';
+output += '\n';
+output += '---------';
+output += '\n';
+output += 'a + b = ' + (a + b);
+output += '\n';
+output += 'a - b = ' + (a - b);
+output += '\n';
+output += 'a * b = ' + (a * b);
+output += '\n';
+output += 'a / b = ' + (a / b);
+output += '\n';
+output += 'a % b = ' + (a % b);
+output += '\n';
+output += '(a += b): ' + (a += b);
+output += '\n';
+output += '(a -= b): ' + (a -= b);
+output += '\n';
+output += '(a *= b): ' + (a *= b);
+output += '\n';
+output += '(a /= b): ' + (a /= b);
+output += '\n';
+output += '(a %= b): ' + (a %= b);
+output += '\n';
+output += '(a == b): ' + (a == b);
+output += '\n';
+output += '(a != b): ' + (a != b);
+output += '\n';
+output += '(a >= b): ' + (a >= b);
+output += '\n';
+output += '(a <= b): ' + (a <= b);
+output += '\n';
+output += '(!a && !c): ' + (!a && !c);
+output += '\n';
+output += '(!a || !c): ' + (!a || !c);
+output += '\n';
+
+alert(output);
+
+//Task 2
+
+let first_name = "Fabrizio";
+let last_name = "Micucci";
+let email = "micu0010@algonquinlive.com";
+let output2 = '';
+
+output2 += `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+alert(output2)
+
+
+
+
